c266b6a713b1dd0fc4deb1cf9bbecf3e818594de
efbonneville <e.f.bonneville@lumc.nl>
2021-01-24 13:50:47 +0100

    Completed benchmarks

--- benchmarking/benchmarks.R
+++ benchmarking/benchmarks.R
@@ -1,13 +1,17 @@
 # Benchmarking - run these on slurm.. with ntasks = 3
 
 # Function to benchmark
+# Load packages
+devtools::load_all()
+
+# Prepare benchmark function
 bench_smcfcs <- function(m, n_cores) {
 
   parlSMCFCS::parlsmcfcs(
-    seed = m, # use m as seed
-    n_cores = n_cores, # number of cores - 1
+    seed = m, # use m as seed, for seq, 2 cores and 3 cores will still be different result
+    n_cores = n_cores, 
     originaldata = smcfcs::ex_compet,
-    cl_type = "FORK", # since running on centos slurm cluster
+    cl_type = "FORK", # Running on centos slurm cluster
     m = m,
     smtype = "compet",
     smformula = list(
@@ -21,6 +25,7 @@ bench_smcfcs <- function(m, n_cores) {
 # Start from 3
 m <- c(3, 5, 10, 15, 25, 50, 100)
 
+# Run benchmarks
 benches <- purrr::map_dfr(
   .x = m,
   .f = ~ {
@@ -30,13 +35,14 @@ benches <- purrr::map_dfr(
       "Sequential" = bench_smcfcs(m = .x, n_cores = 1),
       "2 cores" = bench_smcfcs(m = .x, n_cores = 2),
       "3 cores" = bench_smcfcs(m = .x, n_cores = 3),
-      times = 5 # Set to higher number for more reliable timings
+      times = 5 # Set to higher number for more reliable timings, and assessment of variability
     )
 
     # Bind to summarise
     res <- cbind.data.frame(
       "Cores" = summary(bench)$expr,
-      "Time" = summary(bench)$median,
+      "median_time" = summary(bench)$median,
+      "mean_time" = summary(bench)$mean,
       "m" = .x
     )
 
